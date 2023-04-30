6d7522d49f89eaa7c054c20e7b791f19b433bb2e
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-04-30 14:40:40 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 bonnie
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bonnie Z
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function uses a selection component from https://github.com/CreativeIT/getmdl-select
+ */
+
+var randomNumber = 0
+
+function randomnumbergenerator() {
+  randomNumber = Math.floor(Math.random() * 6) + 1
+}
+
+function myButtonClicked() {
+  // input
+  const numberGuessed = parseInt(
+    document.getElementById("guessed-number").value
+  )
+
+  // process
+  if (numberGuessed == randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "The answer was, " + randomNumber + "!" + "you go it! Good job."
+  }
+
+  //block of code to be execued if condition is true
+  if (numberGuessed !== randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "The answer was, " + randomNumber + "!" + " Nice guess, but try again"
+  }
+}
